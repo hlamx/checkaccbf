20501de9080d887dc6ee40d115120de2437d7dd8
your name <88639120+Mazaeto@users.noreply.github.com>
2022-08-29 10:53:10 +0700

    Update key.lua

--- key.lua
+++ key.lua
@@ -1,3 +1,3 @@
-tablekey = {
-"Mazaeto" }
-return tablekey
+key1 = {
+"wadu","mon","nero","bao","brosa","tungrobo","tanmc","vietpham","zxcvbnm208","duybeo","hckac","huuwf","nhut","hoangbach","honglam","khoikid","ntd","vanh","ducphu","dax"}
+return key1
