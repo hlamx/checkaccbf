f267315b538c743250310f1dc91850c5a05e5e3a
your name <88639120+Mazaeto@users.noreply.github.com>
2022-10-02 12:09:34 +0700

    Update and rename account to account.lua

---
+++ account.lua
@@ -0,0 +1,14 @@
+local acc = {"cogaitoithuong1"
+"choigameaimahack37"
+"choigameaimahack32"
+"choigameaimahack29"
+"choigameaimahack28"
+"choigameaimahack38"
+"choigameaimahack36"
+"choigameaimahack35"
+"choigameaimahack33"
+"cogaitoithuong4"
+"cogaitoithuong3"
+"cogaitoithuong2"
+}
+return acc
